b1fbedd29f677ba1398a16c22c2de8d8eef43e64
Andrew Bancroft <andrewcbancroft@gmail.com>
2015-11-23 13:03:00 -0600

    initial commit

--- SocialSwift/ViewController.swift
+++ SocialSwift/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func postToFacebookTapped(sender: UIButton) {
+        post(toService: SLServiceTypeFacebook)
+    }
 
+    @IBAction func postToTwitterTapped(sender: UIButton) {
+        post(toService: SLServiceTypeTwitter)
+    }
+    
+    func post(toService service: String) {
+        if(SLComposeViewController.isAvailableForServiceType(service)) {
+            let socialController = SLComposeViewController(forServiceType: service)
+            //            socialController.setInitialText("Hello World!")
+            //            socialController.addImage(someUIImageInstance)
+            //            socialController.addURL(someNSURLInstance)
+            self.presentViewController(socialController, animated: true, completion: nil)
+        }
+    }
 }
 
